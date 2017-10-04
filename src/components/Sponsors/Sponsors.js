e8e16b07730c042b71489745fcc17e4cb241e2d1
YaHeng Su <yaheng.su1@gmail.com>
2017-10-03 22:02:31 -0400

    Sponsor page updated

--- src/components/Sponsors/Sponsors.js
+++ src/components/Sponsors/Sponsors.js
@@ -1,22 +1,67 @@
 import React, { Component} from 'react';
-import './Sponsors.css'
+import './Sponsors.css';
+import Paper from 'material-ui/Paper';
+
+//nest paper into cards
+
+
 
 class Sponsors extends Component {
 	render() {
 		return (
-			<div className = 'sponsors'>
-				<div className = 'goldSponsors'>
-					<h1>Google</h1>
-				</div>
+			<div className='container'>
 
-				<div className = 'silverSponsors'>
-				</div>
+			<div className ='goldSponsors'>
+				<p> Gold Sponsors </p>
+					<Paper className ='gold Paper' circle={true} zDepth={4}>
+						<a href='https://www.spotify.com/ca-en/'>
+							<img src='/assets/sponsor_spotify.png' alt='Spotify' />
+						</a>
+					</Paper>
 
-				<div className = 'bronzeSponsors'>
-				</div>
+					<Paper className = 'gold Paper' circle={true} zDepth={4}>
+						<a href='http://www.spacex.com/'>
+							<img src='/assets/sponsor_spacex.png' alt='SpaceX' />
+						</a>
+					</Paper>
+
 			</div>
+
+			<div className='silverSponsors'>
+				<p> Silver Sponsors </p>
+				<Paper className = 'silver Paper' circle={true} zDepth={4}>
+					<a href='http://www.facebook.com/'>
+						<img src='/assets/icon_facebook.svg' alt='Facebook'/>
+					</a>
+				</Paper>
+
+				<Paper className='silver Paper' circle={true}  zDepth={4}>
+					<a href='http://www.twitter.com/'>
+						<img src='/assets/icon_twitter.svg' alt='Twitter' />
+					</a>
+				</Paper>
+			</div>
+
+			<div className='bronzeSponsors'>
+				<p> Bronze Sponsors </p>
+				<Paper className='bronze Paper' circle={true} zDepth={4}>
+					<a href='http://www.instagram.com/'>
+						<img src='/assets/icon_instagram.svg' alt='Instagram' />
+					</a>
+				</Paper>
+
+				<Paper className='bronze Paper' circle={true} zDepth={4}>
+					<a href='http://www.linkedin.com/'>
+						<img src='/assets/icon_linkedin.svg' alt='LinkedIn' />
+					</a>
+				</Paper>
+			</div>
+
+
+			</div>
+
 		);
 	}
 }
 
-export default Sponsors
+export default Sponsors;
