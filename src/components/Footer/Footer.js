4593cef6d86c846cd8a4e910f26d35ee683225b9
YaHeng Su <yaheng.su1@gmail.com>
2017-10-03 23:08:34 -0400

    Footer elements added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Twitter from './Assets/icon_twitter.svg';
+import Instagram from './Assets/icon_instagram.svg';
+import Facebook from './Assets/icon_facebook.svg';
+import Linkedin from './Assets/icon_linkedin.svg';
+
 
 import './Footer.css';
 //back to top Button
@@ -10,8 +15,18 @@ class Footer extends Component {
     return (
       <footer>
         <div id='footer-nav'>
-          <Link to='/'></Link>
-          <Link to='/'>Flock</Link>
+        <a href='https://twitter.com/waterloo_hyper'>
+          <img src={Twitter} alt='Twitter' />
+        </a>
+        <a href='https://www.instagram.com/waterloo_hyper/'>
+          <img src={Instagram} alt='Instagram' />
+        </a>
+        <a href='https://www.facebook.com/waterloohyperloop/'>
+          <img src={Facebook} alt='Facebook' />
+        </a>
+        <a href='https://www.linkedin.com/company-beta/16200816/'>
+          <img src={Linkedin} alt='Linkedin' />
+        </a>
         </div>
       </footer>
     );
