b16cd867eb55ea3b8812471dc4ba0f7921d79719
YaHeng Su <yaheng.su1@gmail.com>
2017-10-04 17:12:32 -0400

    cleaned up

--- src/components/Sponsors/Sponsors.js
+++ src/components/Sponsors/Sponsors.js
@@ -4,6 +4,9 @@ import Paper from 'material-ui/Paper';
 import Spotify from './assets/sponsor_spotify.png';
 import SpaceX from './assets/sponsor_spacex.png';
 import SponsorshipPackage from './assets/sponsorship_package.pdf';
+import Button from 'muicss/lib/react/button';
+import { Link } from 'react-router-dom';
+
 
 class Sponsors extends Component {
 	render() {
@@ -31,13 +34,18 @@ class Sponsors extends Component {
 										<img src={SpaceX} alt='SpaceX' />
 									</a>
 								</Paper>
+								<Paper className ='gold Paper' circle={true} zDepth={1} style={{overflow:'hidden'}}>
+									<a href='https://www.spotify.com/ca-en/'>
+										<img src={Spotify} alt='Spotify' />
+									</a>
+								</Paper>
 						</div>
 
 						<div className='silverSponsors'>
 							<h3> Silver Sponsors </h3>
-							<Paper className = 'silver Paper' circle={true} zDepth={1} style={{overflow:'hidden'}}>
-								<a href='http://www.spacex.com/'>
-									<img src={SpaceX} alt='SpaceX' />
+							<Paper className ='silver Paper' circle={true} zDepth={1} style={{overflow:'hidden'}}>
+								<a href='https://www.spotify.com/ca-en/'>
+									<img src={Spotify} alt='Spotify' />
 								</a>
 							</Paper>
 							<Paper className='silver Paper' circle={true}  zDepth={1} style={{overflow:'hidden'}}>
@@ -45,18 +53,28 @@ class Sponsors extends Component {
 									<img src={SpaceX} alt='SpaceX' />
 								</a>
 							</Paper>
+							<Paper className ='silver Paper' circle={true} zDepth={1} style={{overflow:'hidden'}}>
+								<a href='https://www.spotify.com/ca-en/'>
+									<img src={Spotify} alt='Spotify' />
+								</a>
+							</Paper>
 						</div>
 
 						<div className='bronzeSponsors'>
 							<h3> Bronze Sponsors </h3>
-							<Paper className='bronze Paper' circle={true} zDepth={1} style={{overflow:'hidden'}}>
-								<a href='http://www.spacex.com/'>
-									<img src={SpaceX} alt='SpaceX' />
+							<Paper className ='bronze Paper' circle={true} zDepth={1} style={{overflow:'hidden'}}>
+								<a href='https://www.spotify.com/ca-en/'>
+									<img src={Spotify} alt='Spotify' />
 								</a>
 							</Paper>
 							<Paper className='bronze Paper' circle={true} zDepth={1} style={{overflow:'hidden'}}>
 								<a href='http://www.spacex.com/'>
 									<img src={SpaceX} alt='SpaceX' />
+								</a>
+							</Paper>
+							<Paper className ='bronze Paper' circle={true} zDepth={1} style={{overflow:'hidden'}}>
+								<a href='https://www.spotify.com/ca-en/'>
+									<img src={Spotify} alt='Spotify' />
 								</a>
 							</Paper>
 						</div>
@@ -74,7 +92,8 @@ class Sponsors extends Component {
 									 Send us an email at <a href='mailto:sponsorship@teamwaterloop.ca'>sponsorship@teamwaterloop.ca</a> to learn how you can get involved!
 									 <br></br>
 									 <br></br>
-									 Alternatively, check out our sponsorship package!
+									 Alternatively, check out our sponsorship <a href={SponsorshipPackage}>package</a>.
+									 <br></br>
 								</p>
 							</Paper>
 						</div>
